bd1d25244b2d14d977bbda1340f6c14cdeb14bd2
kin.kim <im@kin.kim>
2020-10-13 17:32:29 +0800

    add :jwt

--- src/test/java/kim/kin/config/ConfigurerJasyptTest.java
+++ src/test/java/kim/kin/config/ConfigurerJasyptTest.java
@@ -7,14 +7,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.sql.DatabaseMetaData;
+
 import static org.junit.Assert.assertEquals;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ConfigurerJasyptTest {
     @Autowired
     ConfigurableEnvironment environment;
+    @Autowired
+    kim.kin.service.Test test;
     @Test
-    public void testEnvironmentProperties() {
+    public void testEnvironmentProperties() throws Exception {
         assertEquals("kinkim", environment.getProperty("spring.datasource.username"));
+
+        test.test();
     }
 }
