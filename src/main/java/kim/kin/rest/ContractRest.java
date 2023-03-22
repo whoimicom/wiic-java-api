244235eb4f48895024f1b5f86dfe640349a468f5
kin.kim <im@kin.kim>
2023-03-22 09:50:35 +0800

    feat: htmlpdf 12414

--- src/main/java/kim/kin/rest/ContractRest.java
+++ src/main/java/kim/kin/rest/ContractRest.java
@@ -30,14 +30,18 @@ public class ContractRest {
     @GetMapping("/contract/{id}")
     @AnonymousKimAccess
     public String contract(@PathVariable String id, Model model) {
-        record Loan(String perMonthDate, String periods, String loanAmt, String borrowReason, String xtSn,
+        record Loan(String downDate, String perMonthDate, String periods, String loanAmt, String borrowReason,
+                    String xtSn,
                     String repaymentAccount,
                     String channel, String repaymentName, String applySn) {
         }
-        record MemberClientele(String creSn, String censusAddrDetail) {
+        record MemberClientele(String creSn, String censusAddrDetail, String bodies, String creValid,
+                               String companyName, String companyAddrDetail,String liveAddrDetail) {
 
         }
-        record Member(String mobile, String email, String gender, String creSn, String realName, Loan loan,
+        record Member(String qq, String nationality, String birthday, String mobile, String email, String gender,
+                      String creSn,
+                      String realName, Loan loan,
                       Map<String, String> paramMap,
                       MemberClientele memberClientele) {
         }
@@ -45,41 +49,59 @@ public class ContractRest {
                               String repaymenMoney) {
 
         }
-        record FundsSource(String companyName) {
+        record FundsSource(String companyName, String companyLogo) {
 
         }
+        record ContactothersHb(String parentsName, String family1Name, String contactName, String parentsTel,
+                               String family1Tel, String contactTel, String parentsRel, String family1Relation,
+                               String contactRel) {
+
+        }
+        HashMap<String, String> paramMap = new HashMap<>();
+        paramMap.put("repaymentBank", "工商银行");
+        paramMap.put("gender", "女");
+        paramMap.put("companyProvince", "重庆市");
+        paramMap.put("censusProvince", "重庆市");
+        paramMap.put("liveProvince", "重庆市");
+        paramMap.put("companyCity", "重庆");
+        paramMap.put("censusCity", "重庆");
+        paramMap.put("liveCity", "重庆");
+        paramMap.put("companyDistrict", "渝北");
+        paramMap.put("censusDistrict", "渝北");
+        paramMap.put("liveDistrict", "渝北");
+        paramMap.put("companyAddrDetail", "**路**街道**号");
+        paramMap.put("censusAddrDetail", "**路**街道**号");
+        paramMap.put("liveAddrDetail", "**路**街道**号");
+        String channel = "08";
 
         RepaymentDates repaymentDate1 = new RepaymentDates("1", "20230101", "5000", "100", "550.00");
         RepaymentDates repaymentDate2 = new RepaymentDates("2", "20230201", "5000", "100", "551.00");
         RepaymentDates repaymentDate3 = new RepaymentDates("3", "20230301", "5000", "100", "552.00");
         List<RepaymentDates> repaymentDates = Arrays.asList(repaymentDate1, repaymentDate2, repaymentDate3);
-        HashMap<String, String> paramMap = new HashMap<>();
-        paramMap.put("repaymentBank", "工商银行");
-        paramMap.put("censusProvince", "重庆");
-        paramMap.put("censusCity", "重庆市");
-        paramMap.put("censusDistrict", "渝北区");
-        String channel = "08";
-        Loan loan = new Loan("1日", "18", "5000", "购买XXXX", "HT22256465********", "6226***********", channel, "麻子", "APPLYSNXXX***");
-        MemberClientele memberClientele = new MemberClientele("XXXXCRESN", "金山商业中心");
-        Member member = new Member("18555555555", "@5dhj.com", "1", "3333333CRESN", "麻子", loan, paramMap, memberClientele);
 
+        Loan loan = new Loan("20230101", "1日", "18", "5000", "购买XXXX", "HT22256465********", "6226***********", channel, "麻子", "APPLYSNXXX***");
+        MemberClientele memberClientele = new MemberClientele("XXXXCRESN", "金山商业中心", "渝北", "20550101", "**公司", "**路**街道**号","**路**街道**号");
+        Member member = new Member("QQ*****", "汉", "20230101", "18555555555", "@5dhj.com", "1", "3333333CRESN", "麻子", loan, paramMap, memberClientele);
+        FundsSource fundsSource = new FundsSource("hoben-api", "http://hw.5dhj.com/huij-fs/file/common/app/images/logo_10_tz.png");
+        ContactothersHb contactothersHb = new ContactothersHb("麻大", "麻三", "麻烦", "185*******", "185*******", "185*******", "父子", "母子", "朋友 ");
         model.addAttribute("member", member);
         model.addAttribute("repaymentDates", repaymentDates);
         model.addAttribute("monthRat", "0.15%");
-        model.addAttribute("fundsSource", new FundsSource("hoben-api"));
+        model.addAttribute("fundsSource", fundsSource);
         String currentDate = LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE);
         model.addAttribute("currDate", currentDate);
         model.addAttribute("currYear", currentDate.substring(0, 4));
         model.addAttribute("currMonth", currentDate.substring(4, 6));
         model.addAttribute("currDay", currentDate.substring(6, 8));
+        model.addAttribute("contactothersHb", contactothersHb);
 //        return "pdf_template12403.html";
         return "pdf_template" + id + ".html";
     }
 
-    @GetMapping("/gen")
+    @GetMapping("/gen/{id}")
     @AnonymousKimAccess
     @ResponseBody
-    public String gen(Model model) {
+    public String gen(@PathVariable String id, Model model) {
         int nano = LocalDateTime.now().getSecond();
         try (OutputStream os = new FileOutputStream("d:\\contract.pdf")) {
             PdfRendererBuilder builder = new PdfRendererBuilder();
@@ -87,7 +109,7 @@ public class ContractRest {
 //            InputStream inputStream = classPathResource.getInputStream();
             builder.useFont(classPathResource.getFile(), "SimSun");
             builder.useFastMode();
-            builder.withUri("http://localhost:1987/kim-api/contract/12404");
+            builder.withUri("http://localhost:1987/kim-api/contract/"+id);
 //            builder.withHtmlContent(Files.readString(Paths.get("d:\\pdf.html")), "d:\\pdf.html");
             builder.toStream(os);
             builder.run();
