5cf2c80a043ce2c233aa33e6a2121072cc6eb01d
whoimi <mail@whoimi.com>
2023-03-30 22:25:38 +0800

    feat: SimSun.ttf

--- src/main/java/kim/kin/utils/PdfUtils.java
+++ src/main/java/kim/kin/utils/PdfUtils.java
@@ -1,23 +1,34 @@
 package kim.kin.utils;
 
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.ResourceUtils;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-
-import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
-import org.springframework.core.io.ClassPathResource;
 
 public class PdfUtils {
     public static void main(String[] args) throws Exception {
         try (OutputStream os = new FileOutputStream("d:\\out.pdf")) {
             PdfRendererBuilder builder = new PdfRendererBuilder();
             builder.useFastMode();
-            ClassPathResource classPathResource = new ClassPathResource("fonts/SimSun.ttf");
-//            InputStream inputStream = classPathResource.getInputStream();
-            builder.useFont(classPathResource.getFile(), "SimSun");
+            String userHome = System.getProperty("user.home");
+            String fontPath = userHome + "/SimSun.ttf";
+            Path path = Paths.get(fontPath);
+            if (!Files.exists(path)) {
+                ClassPathResource classPathResource = new ClassPathResource("fonts/SimSun.ttf");
+                InputStream inputStream = classPathResource.getInputStream();
+                Files.copy(inputStream, path);
+            }
+
+            builder.useFont(new File(fontPath), "SimSun");
+//            builder.useFont(ResourceUtils.getFile("classpath:fonts/SimSun.ttf"), "SimSun");
+
 //            builder.withUri("https://zditect.com/main-advanced/java/java-html-to-pdf.html");
             builder.withHtmlContent(Files.readString(Paths.get("d:\\pdf.html")), "d:\\pdf.html");
             builder.toStream(os);
