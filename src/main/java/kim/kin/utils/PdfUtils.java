6ac0ed87529cea76a3b50af95cd76a1c399882df
whoimi <mail@whoimi.com>
2023-09-15 15:31:55 +0800

    fix: fontFamily

--- src/main/java/kim/kin/utils/PdfUtils.java
+++ src/main/java/kim/kin/utils/PdfUtils.java
@@ -1,6 +1,7 @@
 package kim.kin.utils;
 
 import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
@@ -13,19 +14,23 @@ import java.nio.file.Paths;
 
 @Component
 public class PdfUtils {
+    @Value("${kim.kin.font-family}")
+    private String fontFamily;
 
     public void generatePdf(String htmlUrl, String outputPath) throws IOException {
+//        String fontFamily = "Alibaba-PuHuiTi-Light";
+//        String fontFamily = "SimSun";
         try (OutputStream os = new FileOutputStream(outputPath)) {
             PdfRendererBuilder pdfRendererBuilder = new PdfRendererBuilder();
-            String fontPath = System.getProperty("user.home") + "/SimSun.ttf";
+            String fontPath = System.getProperty("user.home") + "/" + fontFamily + ".ttf";
             Path path = Paths.get(fontPath);
             if (!Files.exists(path)) {
-                ClassPathResource classPathResource = new ClassPathResource("fonts/SimSun.ttf");
+                ClassPathResource classPathResource = new ClassPathResource("fonts/" + fontFamily + ".ttf");
                 try (InputStream inputStream = classPathResource.getInputStream()) {
                     Files.copy(inputStream, path);
                 }
             }
-            pdfRendererBuilder.useFont(new File(fontPath), "SimSun");
+            pdfRendererBuilder.useFont(new File(fontPath), fontFamily);
             pdfRendererBuilder.useFastMode();
             pdfRendererBuilder.withUri(htmlUrl);
             pdfRendererBuilder.toStream(os);
@@ -41,14 +46,14 @@ public class PdfUtils {
             String fontPath = userHome + "/SimSun.ttf";
             Path path = Paths.get(fontPath);
             if (!Files.exists(path)) {
-                ClassPathResource classPathResource = new ClassPathResource("fonts/SimSun.ttf");
+                ClassPathResource classPathResource = new ClassPathResource("fonts/Alibaba-PuHuiTi-Light.ttf");
                 try (InputStream inputStream = classPathResource.getInputStream()) {
                     Files.copy(inputStream, path);
                 }
             }
 
             builder.useFont(new File(fontPath), "SimSun");
-            File file = ResourceUtils.getFile("classpath:fonts/SimSun.ttf");
+            File file = ResourceUtils.getFile("classpath:fonts/Alibaba-PuHuiTi-Light.ttf");
 
             builder.useFont(file, "SimSun");
 
