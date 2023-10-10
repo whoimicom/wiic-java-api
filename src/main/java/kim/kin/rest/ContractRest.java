ab35f1d555fb671add896d911e8e339c723a457b
whoimi <mail@whoimi.com>
2023-10-10 17:14:33 +0800

    refact: TEMP

--- src/main/java/kim/kin/rest/ContractRest.java
+++ src/main/java/kim/kin/rest/ContractRest.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
@@ -36,105 +35,63 @@ public class ContractRest {
      * 查看模板
      * <a href="http://localhost:1987/kim-api/contract/12405">...</a>
      *
-     * @param id    模板编号
-     * @param model model
+     * @param tempName 模板名称
+     * @param model    model
      * @return 模板
      */
-    @GetMapping("/contract/{id}")
+    @GetMapping("/contract/{tempName}")
     @AnonymousKimAccess
-    public String contract(@PathVariable String id, Model model) {
-        record Loan1(String downDate, String perMonthDate, String periods, String loanAmt, String borrowReason,
-                     String xtSn, String repaymentAccount, String channel, String repaymentName, String applySn,
-                     String fundsSn) {
-        }
-        record MemberClientele(String creSn, String censusAddrDetail, String bodies, String creValid,
-                               String companyName, String companyAddrDetail, String liveAddrDetail) {
-
-        }
-        record Member(String applyTime, String qq, String nationality, String birthday, String mobile, String email,
-                      String gender,
-                      String creSn, String realName, Loan1 loan, Map<String, String> paramMap,
-                      MemberClientele memberClientele) {
-        }
-        record RepaymentDates(String period, String repaymenDate, String planPrincipal, String planServiceFee,
-                              String repaymenMoney) {
-        }
-        record FundsSource1(String companyName, String companyLogo) {
-
-        }
-        record ContactothersHb(String parentsName, String family1Name, String contactName, String parentsTel,
-                               String family1Tel, String contactTel, String parentsRel, String family1Relation,
-                               String contactRel) {
-        }
+    public String contract(@PathVariable String tempName, Model model) {
+//        FundsSource1 fundsSource = new FundsSource1("hoben-api", "https://hw.5dhj.com/huij-fs/file/common/app/images/logo_10_tz.png");
         String currentDate = LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE);
-        HashMap<String, String> paramMap = new HashMap<>();
-        paramMap.put("repaymentBank", "工商银行");
-        paramMap.put("gender", "女");
-        paramMap.put("companyProvince", "重庆市");
-        paramMap.put("censusProvince", "重庆市");
-        paramMap.put("liveProvince", "重庆市");
-        paramMap.put("companyCity", "重庆");
-        paramMap.put("censusCity", "重庆");
-        paramMap.put("liveCity", "重庆");
-        paramMap.put("companyDistrict", "渝北");
-        paramMap.put("censusDistrict", "渝北");
-        paramMap.put("liveDistrict", "渝北");
-        paramMap.put("companyAddrDetail", "**路**街道**号");
-        paramMap.put("censusAddrDetail", "**路**街道**号");
-        paramMap.put("liveAddrDetail", "**路**街道**号");
-        String channel = "08";
-
-        RepaymentDates repaymentDate1 = new RepaymentDates("1", "20230101", "5000", "100", "550.00");
-        RepaymentDates repaymentDate2 = new RepaymentDates("2", "20230201", "5000", "100", "551.00");
-        RepaymentDates repaymentDate3 = new RepaymentDates("3", "20230301", "5000", "100", "552.00");
-        List<RepaymentDates> repaymentDates = Arrays.asList(repaymentDate1, repaymentDate2, repaymentDate3);
-
-        Loan1 loan = new Loan1("20230101", "1日", "18", "5000", "购买XXXX", "HT22256465********", "6226***********", channel, "麻子", "APPLYSNXXX***", "131");
-        MemberClientele memberClientele = new MemberClientele("XXXXCRESN", "金山商业中心", "渝北", "20550101", "**公司", "**路**街道**号", "**路**街道**号");
-        Member member = new Member(currentDate, "QQ*****", "汉", "20230101", "18555555555", "@5dhj.com", "1", "3333333CRESN", "麻子", loan, paramMap, memberClientele);
-        FundsSource1 fundsSource = new FundsSource1("hoben-api", "https://hw.5dhj.com/huij-fs/file/common/app/images/logo_10_tz.png");
-        ContactothersHb contactothersHb = new ContactothersHb("麻大", "麻三", "麻烦", "185*******", "185*******", "185*******", "父子", "母子", "朋友 ");
-        model.addAttribute("member", member);
-        model.addAttribute("repaymentDates", repaymentDates);
-        model.addAttribute("loan.monthRate", "0.15%");
-        model.addAttribute("fundsSource", fundsSource);
-
-        model.addAttribute("contractDate", currentDate);
-        model.addAttribute("contractYear", currentDate.substring(0, 4));
-        model.addAttribute("contractMonth", currentDate.substring(4, 6));
-        model.addAttribute("contractDay", currentDate.substring(6, 8));
-        model.addAttribute("contactothersHb", contactothersHb);
-        model.addAttribute("srviceRat", "3%");
-        model.addAttribute("yearRat", "18%");
-        model.addAttribute("LoanAmtUp", "伍仟");
-        model.addAttribute("contractEndDate", "20230301");
-        model.addAttribute("name", "麻三");
-        model.addAttribute("creSn", "XXXXCRESN");
-        model.addAttribute("bankCardNo", "6226***********");
-        model.addAttribute("bankDesc", "工行银行");
-
-        record Customer(String name, String genger, String birthday, String tel, String qq, String email,
+        record Customer(String name, String gender, String birthday, String tel, String qq, String email,
                         String idNo, String idProvince, String idCity,
-                        String idDistrict, String idAddrDetail, String idIssuePlace,
+                        String idDistrict, String idAddrDetail, String idIssuePlace, String idValidity,
                         String liveProvince, String liveCity, String liveDistrict, String liveAddrDetail,
-                        String idValidity, String nation, String companyName, String companyProvince,
+                        String nation, String companyName, String companyProvince,
                         String companyCity, String companyDistrict, String companyAddrDetail) {
         }
+        Customer customer = new Customer("KINKIM", "M", "20231010", "185****0088", "198799**", "im@kin.kim",
+                "511586xxxxxx023354", "四川", "广安", "XX县", "**路**街道**号",
+                "XXX县", "20991231", "重庆", "重庆", "重庆", "**路**街道**号",
+                "汉", "非自然现象研究院", "重庆", "重庆", "XXX北", "**路**街道**号");
         record Contact(String name, String tel, String relation) {
         }
+        List<Contact> contacts = Arrays.asList(
+                new Contact("A", "177****7777", "A关系"),
+                new Contact("B", "199****7777", "B关系"),
+                new Contact("C", "133****7777", "C关系")
+        );
+
         record Loan(String id, String amount, String amountUp, String periods, String purpose, String monthRate,
                     String repaymentMethod,
                     String firstPayDate, String perPayDate, String repaymentName, String repaymentAccount,
                     String repaymentBank) {
         }
-        record FundsSource(String name, String shortName, String logoAddr, String bankName, String bankAccount,
-                           String legalRepresentative, String registrationAddr, String tel, String contactAddr) {
+        Loan loan = new Loan("LOAN" + currentDate, "6000", "六千元整", "6", "消费", "0.35%",
+                "等额本息", "20231010", "1010", "KINKIM", "2423450923845", "平安银行");
+        record Capital(String name, String shortName, String logoAddr, String bankName, String bankAccount,
+                       String legalRepresentative, String registrationAddr, String tel, String contactAddr) {
 
         }
-        record Repayment(String stages, String date, String mount) {
+        Capital capital = new Capital("XX融资担保有限公司", "担保公司", "https://asdasd.png", "XX银行", "33333333****333333", "XX法人", "XX省XX市", "0826-*****", "XX省XX市");
+        record Repayment(String stages, String date, String principal, String serviceFee, String monthlyPayment) {
 
         }
+        List<Repayment> repayments = Arrays.asList(
+                new Repayment("1", "20231010", "500", "50", "550"),
+                new Repayment("2", "20231010", "500", "50", "550"),
+                new Repayment("3", "20231010", "500", "50", "550"),
+                new Repayment("4", "20231010", "500", "50", "550"),
+                new Repayment("5", "20231010", "500", "50", "550"),
+                new Repayment("6", "20231010", "500", "50", "550")
+        );
 
+        model.addAttribute("customer", customer);
+        model.addAttribute("contacts", contacts);
+        model.addAttribute("loan", loan);
+        model.addAttribute("repayments", repayments);
+        model.addAttribute("capital", capital);
         model.addAttribute("currentDate", currentDate);
         model.addAttribute("contractDate", currentDate);
         model.addAttribute("contractEndDate", LocalDate.now().plusDays(180));
@@ -148,48 +105,51 @@ public class ContractRest {
 
         model.addAttribute("fontFamily", fontFamily);
 //        return "pdf_template12403.html";
-        String temp = "pdf_template" + id + ".html";
+        String temp = "TEMP-" + tempName + ".html";
         System.out.println(temp);
         return temp;
     }
 
     /**
      * 生成根据模板生成PDF
-     * <a href="http://localhost:1987/kim-api/genpdf/12405">...</a>
+     * <a href="http://localhost:1987/kim-api/generateByName/ApplicationForm">...</a>
      *
-     * @param id 模板编号
+     * @param tempName 模板名称
      * @return 路径
      */
-    @GetMapping("/gen/{id}")
+    @GetMapping("/generateByName/{tempName}")
     @AnonymousKimAccess
     @ResponseBody
-    public String genpdf(@PathVariable String id) throws IOException {
-        String uri = "http://localhost:1987/kim-api/contract/" + id;
+    public String generateByName(@PathVariable String tempName) throws IOException {
+        String uri = "http://localhost:1987/kim-api/contract/" + tempName;
         int nextInt = new Random().nextInt();
-        String outputPath = "d:\\contract\\" + id + "-" + fontFamily + "-" + nextInt + ".pdf";
+        String outputPath = "d:\\contract\\" + tempName + "-" + fontFamily + "-" + nextInt + ".pdf";
         pdfUtils.generatePdf(uri, outputPath);
-        return outputPath + "  pdf_template" + id + ".html";
+        return outputPath + "  TEMP-" + tempName + ".html";
     }
 
-    @GetMapping("/generatePdf")
+    /**
+     * <a href="http://localhost:1987/kim-api/generateAll">...</a>
+     */
+    @GetMapping("/generateAll")
     @AnonymousKimAccess
     @ResponseBody
-    public String generatePdf() {
-        int nano = LocalDateTime.now().getSecond();
-        List<String> list = Arrays.asList("12403", "12404", "12405", "12406", "12407", "12412", "12413", "12414", "12415", "12417");
+    public void generateAll() {
+        List<String> list = Arrays.asList("ApplicationForm", "AuthorizationLetter", "AuthorizationLetter-Credit",
+                "AuthorizationLetter-Person", "CertificateOfIndebtedness", "ConfirmationLetter", "DelegationDeduction",
+                "LetterOfAssignment", "LoanContract", "RepaymentGuide", "ServiceContract");
         Random random = new Random();
         int nextInt = random.nextInt();
         int size = Math.abs(nextInt % list.size());
-        String id = list.get(size);
-        String uri = "http://localhost:1987/kim-api/contract/" + id;
-        String outputPath = "d:\\contract\\" + id + "-" + fontFamily + "-" + nextInt + ".pdf";
-        try (OutputStream ignored = new FileOutputStream(outputPath)) {
-            pdfUtils.generatePdf(uri, outputPath);
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-        System.out.println(LocalDateTime.now().getSecond() - nano);
-        return outputPath;
+//        String tempName = list.get(size);
+        list.forEach(tempName -> {
+            String uri = "http://localhost:1987/kim-api/contract/" + tempName;
+            String outputPath = "d:\\contract\\" + tempName + "-" + fontFamily + "-" + nextInt + ".pdf";
+            try (OutputStream ignored = new FileOutputStream(outputPath)) {
+                pdfUtils.generatePdf(uri, outputPath);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
